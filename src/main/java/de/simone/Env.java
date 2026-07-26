d629f371a752520ebb10a728420844d6b685e9b5
sauersitrone <sauer.sitrone@gmail.com>
2026-07-26 07:40:05 +0200

    backup

--- src/main/java/de/simone/Env.java
+++ src/main/java/de/simone/Env.java
@@ -2,30 +2,29 @@ package de.simone;
 
 import java.util.Properties;
 
-public class Env extends Properties{
+public class Env extends Properties {
 
     public static String chaosLauncherPath;
     public static boolean isParamTweaker;
     public static boolean isTesting;
     public static boolean fogOfWar;
 
-    
     /**
      * Disabling makes game so fast, you actually be like "Daaaaamn!".
      * Unfortunately it means nothing gets rendered, so game appears to be frozen.
      */
     public static boolean DISABLE_GUI = false;
 
-    //    public static boolean USE_CODE_PROFILER = true;
+    // public static boolean USE_CODE_PROFILER = true;
     public static boolean USE_CODE_PROFILER = false;
 
     public static void init() {
         new Env();
     }
-    
+
     private Env() {
         try {
-            load(Main.class.getResourceAsStream("/resources/config.properties"));
+            load(Env.class.getClassLoader().getResourceAsStream("./config.properties"));
             chaosLauncherPath = getProperty("chaosLauncherPath");
             isParamTweaker = Boolean.parseBoolean(getProperty("isParamTweaker"));
             isTesting = Boolean.parseBoolean(getProperty("isTesting"));
@@ -33,5 +32,5 @@ public class Env extends Properties{
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }    
+    }
 }

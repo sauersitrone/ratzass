46274c0cd316ec61195c44e01c66cb67401775eb
sauersitrone <sauer.sitrone@gmail.com>
2026-07-28 18:58:15 +0200

    backup

---
+++ src/main/java/de/simone/game/actions/BuildOrder.java
@@ -0,0 +1,15 @@
+package de.simone.game.actions;
+
+import java.util.List;
+
+public class BuildOrder {
+
+    private String id;
+    
+    private List<StarCraftAction> actions;
+    
+    public BuildOrder() {
+        //
+    }
+
+}

6cc626debd885f0ade889a5992616e347378ba07
sauersitrone <sauer.sitrone@gmail.com>
2026-09-04 12:33:11 +0200

    backup

--- src/main/java/de/simone/btree/IsResourceNeededTask.java
+++ src/main/java/de/simone/btree/IsResourceNeededTask.java
@@ -1,24 +1,14 @@
 package de.simone.btree;
 
-import bwapi.UnitType;
 import de.simone.RBWListener;
-import de.simone.command.BuildOrder;
-import de.simone.command.LogisticCenter;
 import de.simone.command.StarCraftConstants;
-import de.simone.command.StarCraftConstants.OrderPriority;
 
 public class IsResourceNeededTask extends RTask {
 
     @Override
     public Status execute() {
         if (RBWListener.currentSupplyTotal - RBWListener.currentSupplyUsed < StarCraftConstants.TERRAN_MIN_SUPPLY) {
-        return Status.SUCCEEDED;
-
-            // BuildOrder buildOrder = new BuildOrder(getName(), UnitType.Terran_Supply_Depot, 1);
-            // buildOrder.priority = OrderPriority.High;
-            // LogisticCenter.getInstance().addBuildOrder(buildOrder);
-
-            // CommandQueue.getInstance().build(UnitType.Terran_Supply_Depot);
+            return Status.SUCCEEDED;
         }
 
         return Status.FAILED;

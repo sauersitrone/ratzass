600bc2591d465741bba989235525310d7f4bbdeb
sauersitrone <sauer.sitrone@gmail.com>
2026-09-14 18:43:01 +0200

    backup

--- src/main/java/de/simone/StarCraftException.java
+++ src/main/java/de/simone/StarCraftException.java
@@ -1,5 +1,8 @@
 package de.simone;
 
+/**
+ * Simple exception class for this project.
+ */
 public class StarCraftException extends RuntimeException {
 
     public StarCraftException(String msg) {

54f809794c9d2eab1a509f61fa02e4d991d50156
sauersitrone <sauer.sitrone@gmail.com>
2026-09-26 18:27:24 +0200

    backup

--- src/main/java/de/simone/btree/Blackboard.java
+++ src/main/java/de/simone/btree/Blackboard.java
@@ -1,14 +1,5 @@
 package de.simone.btree;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
-import de.simone.command.BuildOrder;
-
 public class Blackboard {
-    
-    public Map<String, List<BuildOrder>> orders = new TreeMap<>();
-
 
 }
